1ae86d9094ca3b8e95cd12118b3e043d08c63e95
Yanice Slegers <yaniceslegers@gmail.com>
2018-03-23 10:21:21 +0100

    Laravel title changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel ss
                 </div>
 
                 <div class="links">
