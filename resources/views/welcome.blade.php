b88b1283170defeb8bf80884b4183183fd555dc8
Yanice Slegers <yaniceslegers@gmail.com>
2018-03-23 10:18:46 +0100

    Yanice edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Yanice is Leuk
                 </div>
 
                 <div class="links">
