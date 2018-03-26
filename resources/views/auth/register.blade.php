eab2a00b6ff5ad4bf943294940b15ac5972be009
Yanice Slegers <yaniceslegers@gmail.com>
2018-03-26 10:16:30 +0200

    Added GDPR to register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,7 +60,13 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="gdpr" class="col-md-4 col-form-label text-md-right">{{ __('Accept the GDPR policy') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="gdpr-checkbox" type="checkbox" class="form-control" name="gdpr">
+                            </div>
+                        </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
