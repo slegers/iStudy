18ae883da0ab9a723a38897ca7f56877153311ef
Yanice Slegers <yaniceslegers@gmail.com>
2018-03-30 16:50:23 +0200

    studymoment migration added

---
+++ database/migrations/2018_03_30_084213_studymoments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Studymoments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studymoments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('course_id');
+            $table->boolean('in_class');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studymoments');
+
+    }
+}
